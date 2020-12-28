fc6d0c862afed0515f27bb459d8d0990b977555d
amuss2003 <amuss2003@yahoo.com>
2020-12-28 13:47:16 +0200

    test

--- TestApp/Controllers/WeatherForecastController.cs
+++ TestApp/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace TestApp.Controllers
             return Enumerable.Range(1, 3).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-20, 50),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
